baa21be70c9665b0913550465347cc98cbf5c855
unknown <lhn1208@naver.com>
2025-05-16 23:50:20 +0900

    업로드

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/thai-mts-app/',
+  base: '/thai-mts/',
   css: {
     preprocessorOptions: {
       scss: {
